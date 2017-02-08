20c2a68a96ffa569db9e28ebec9f90568b64025a
Fabian Scheidt <fabianscheidt@yahoo.de>
2017-02-08 21:17:29 +0100

    Bugfix

--- ng2-query-input.ts
+++ ng2-query-input.ts
@@ -1,6 +1,5 @@
 export * from './src/query-input/model/query'
 export * from './src/query-input/model/query-category'
-export * from './src/query-input/model/query-input-delegate'
 export * from './src/query-input/model/query-part'
 export * from './src/query-input/query.service'
 export * from './src/query-input/query-input.component'
