0298a34ce8af771c6a5ae6a703c291f1864ac3c3
Fabian Scheidt <fabianscheidt@yahoo.de>
2017-02-23 00:34:59 +0100

    Parts of the query are now highlighted in the input

--- src/query-input/model/query-part.ts
+++ src/query-input/model/query-part.ts
@@ -8,4 +8,11 @@ export class QueryPart {
     if(typeof category !== 'undefined') this.category = category;
     if(typeof value !== 'undefined') this.value = value;
   }
+
+  public toString(): string {
+    let category = this.category ? this.category.name + ": " : "";
+    let value = this.value || "";
+    value = value.indexOf(" ") == -1 ? value : '"' + value + '"';
+    return category + value;
+  }
 }
